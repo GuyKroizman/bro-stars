91f287e3572758dc3c8452b0c7f17dc42655ea9d
GuyKroizman <guykr@wix.com>
2024-07-26 23:29:57 +0300

    adding mmorpg

--- scenegameover.lua
+++ scenegameover.lua
@@ -1,13 +1,15 @@
 sceneGameOver = {
-    update = function()
-        if btnp(4) then
-            _update = sceneGame.update
-            _draw = sceneGame.draw
-            bro.alive = true
-        end
-    end,
-    draw = function()
-        cls()
-        print("GAME OVER BRO", 20, 64)
-    end
+	update = function()
+		if btnp(4) then
+			_update = sceneGame.update
+			_draw = sceneGame.draw
+			bro.alive = true
+			enemies = {}
+		end
+	end,
+	draw = function()
+		cls()
+		print("GAME OVER BRO", 20, 64)
+	end,
 }
+
